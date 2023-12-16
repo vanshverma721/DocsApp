43593d0ada8a1e932fdf734045e7cd8665e95e6f
vanshverma721 <vvv.721781@gmail.com>
2023-12-16 22:41:09 +0530

    added code

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 function App() {
   return (
-    <div className='w-full h-screen bg-zinc-800'>
-      <h1>Doc</h1>
+    <div className=' relative w-full h-screen bg-zinc-800'>
+      <div className='absolute top-[5%] w-full py-10 flex justify-center text-zinc-600 text-xl font-semibold'>Documents.</div>
+      <h1 className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[13vw] leading-none tracking-tighter font-semibold text-zinc-900'>Docs.</h1>
     </div>
   )
 }
