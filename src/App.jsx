d314d5e4a0d1aa10a2979242f41e4e198390318f
vanshverma721 <vvv.721781@gmail.com>
2023-12-16 22:57:27 +0530

    updated app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
+import Background from "./components/Background"
 
 function App() {
   return (
     <div className=' relative w-full h-screen bg-zinc-800'>
-      <div className='absolute top-[5%] w-full py-10 flex justify-center text-zinc-600 text-xl font-semibold'>Documents.</div>
-      <h1 className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[13vw] leading-none tracking-tighter font-semibold text-zinc-900'>Docs.</h1>
+      <Background />
     </div>
   )
 }
