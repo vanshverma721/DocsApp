e029d82d47f88af165b9e30371f6e5fc107b6fc3
vanshverma721 <vvv.721781@gmail.com>
2023-12-28 15:27:59 +0530

    updated

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,28 @@
 import React from 'react'
 import { FaRegFileAlt } from "react-icons/fa";
 import { LuDownload } from "react-icons/lu";
+import { IoClose } from "react-icons/io5";
 
-const Card = () => {
+const Card = ({ data }) => {
     return (
         <div className='relative w-60 h-72 rounded-[45px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden'>
             <FaRegFileAlt />
-            <p className='text-sm leading-tight mt-5 font-semibold'>Lorem ipsum, dolor sit amet consectetur adipisicing.</p>
+            <p className='text-sm leading-tight mt-5 font-semibold'>{data.desc}</p>
             <div className='footer absolute bottom-0 w-full left-0'>
                 <div className='flex items-center justify-between px-8 py-3 mb-3'>
-                    <h5>.4mb</h5>
+                    <h5>{data.filessize}</h5>
                     <span className='w-7 h-7 bg-zinc-600 rounded-full flex items-center justify-center'>
-                        <LuDownload size=".7em" color='#fff' />
+                        {data.close ? <IoClose /> : <LuDownload size=".7em" color='#fff' />}
                     </span>
                 </div>
-                <div className='tag w-full py-4 bg-green-600 flex items-center justify-center'>
-                    <h3 className='text-sm font-semibold'>Download Now</h3>
-                </div>
+                {
+                    data.tag.isOpen && (
+                        <div className='tag w-full py-4 bg-green-600 flex items-center justify-center'>
+                            <h3 className='text-sm font-semibold'>Download Now</h3>
+                        </div>
+                    )
+                }
+
             </div>
         </div>
     )
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -2,11 +2,23 @@ import React from 'react'
 import Card from './Card'
 
 const Foreground = () => {
-  return (
-    <div className='fixed top-0 left-0 z-[3] w-full h-full'>
-        <Card />
-    </div>
-  )
+
+    const data = [
+        {
+            desc: "I have to finish my office work today at any cost.",
+            filessize: ".9mb",
+            close: true,
+            tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green" }
+        }
+    ];
+
+    return (
+        <div className='fixed top-0 left-0 z-[3] w-full h-full'>
+            {data.map((item, index) => (
+                <Card data={item} />
+            ))}
+        </div>
+    )
 }
 
 export default Foreground
